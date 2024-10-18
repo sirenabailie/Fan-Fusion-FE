e8379e1622db7cd1cb56a59f166cbbb487040756
Casey <dinnerdoggy@gmail.com>
2024-10-17 19:09:53 -0500

    create function working properly at least with firebase

--- src/components/forms/StoryForm.js
+++ src/components/forms/StoryForm.js
@@ -15,7 +15,7 @@ const initialState = {
   image: '',
   dateCreated: false,
   targetAudience: '',
-  categoryId: '',
+  categoryfirebaseKey: '',
 };
 
 function StoryForm({ obj = initialState }) {
@@ -25,7 +25,7 @@ function StoryForm({ obj = initialState }) {
   const { user } = useAuth();
 
   useEffect(() => {
-    getStories(user.uid).then(setStories);
+    getStories(user.ufirebaseKey).then(setStories);
 
     if (obj.firebaseKey) setFormInput(obj);
   }, [obj, user]);
@@ -43,7 +43,7 @@ function StoryForm({ obj = initialState }) {
     if (obj.firebaseKey) {
       updateStory(formInput).then(() => router.push(`/Story/${obj.firebaseKey}`));
     } else {
-      const payload = { ...formInput, uid: user.uid };
+      const payload = { ...formInput, ufirebaseKey: user.ufirebaseKey };
       createStory(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
         updateStory(patchPayload).then(() => {
@@ -58,52 +58,31 @@ function StoryForm({ obj = initialState }) {
       <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Story</h2>
 
       {/* IMAGE INPUT  */}
-      <FloatingLabel controlId="floatingInput2" label="Cover Image" className="mb-3">
+      <FloatingLabel controlfirebaseKey="floatingInput2" label="Cover Image" className="mb-3">
         <Form.Control type="url" placeholder="Enter an image url" name="image" value={formInput.image} onChange={handleChange} required />
       </FloatingLabel>
 
       {/* TITLE INPUT  */}
-      <FloatingLabel controlId="floatingInput1" label="Story Title" className="mb-3">
+      <FloatingLabel controlfirebaseKey="floatingInput1" label="Story Title" className="mb-3">
         <Form.Control type="text" placeholder="Enter a title for your story" name="title" value={formInput.title} onChange={handleChange} required />
       </FloatingLabel>
 
-      {/* PRICE INPUT  */}
-      <FloatingLabel controlId="floatingInput3" label="Book Price" className="mb-3">
-        <Form.Control type="text" placeholder="Enter price" name="price" value={formInput.price} onChange={handleChange} required />
+      {/* DESCRIPTION TEXTAREA  */}
+      <FloatingLabel controlfirebaseKey="floatingTextarea" label="Description" className="mb-3">
+        <Form.Control as="textarea" placeholder="Description" style={{ height: '100px' }} name="description" value={formInput.description} onChange={handleChange} required />
       </FloatingLabel>
 
-      {/* AUTHOR SELECT  */}
-      <FloatingLabel controlId="floatingSelect" label="Author">
-        <Form.Select aria-label="Author" name="author_id" onChange={handleChange} className="mb-3" value={formInput.author_id || ''} required>
-          <option value="">Select an Author</option>
-          {stories.map((author) => (
-            <option key={author.firebaseKey} value={author.firebaseKey}>
-              {author.first_name} {author.last_name}
+      {/* TARGET AUDIENCE  */}
+      <FloatingLabel controlfirebaseKey="floatingSelect" label="Target Audience">
+        <Form.Select aria-label="Target Audience" name="targetAudience" onChange={handleChange} className="mb-3" value={formInput.targetAudience || ''} required>
+          <option value="">Select an Audience</option>
+          {stories.map((story) => (
+            <option key={story.firebaseKey} value={story.firebaseKey}>
+              {story.targetAudience}
             </option>
           ))}
         </Form.Select>
       </FloatingLabel>
-
-      {/* DESCRIPTION TEXTAREA  */}
-      <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
-        <Form.Control as="textarea" placeholder="Description" style={{ height: '100px' }} name="description" value={formInput.description} onChange={handleChange} required />
-      </FloatingLabel>
-
-      {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
-      <Form.Check
-        className="text-white mb-3"
-        type="switch"
-        id="sale"
-        name="sale"
-        label="On Sale?"
-        checked={formInput.sale}
-        onChange={(e) => {
-          setFormInput((prevState) => ({
-            ...prevState,
-            sale: e.target.checked,
-          }));
-        }}
-      />
 
       {/* SUBMIT BUTTON  */}
       <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Story</Button>
@@ -115,11 +94,9 @@ StoryForm.propTypes = {
   obj: PropTypes.shape({
     description: PropTypes.string,
     image: PropTypes.string,
-    price: PropTypes.string,
-    sale: PropTypes.bool,
     title: PropTypes.string,
-    author_id: PropTypes.string,
-    firebaseKey: PropTypes.string,
+    targetAudience: PropTypes.string,
+    firebaseKey: PropTypes.number,
   }),
 };
 
