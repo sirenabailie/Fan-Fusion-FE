5cb4bb6710b522a93981b684bcf66c7835e89028
Casey <dinnerdoggy@gmail.com>
2024-10-24 20:32:57 -0500

    delete working, WOOT

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -5,8 +5,17 @@ import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
+import { deleteStory } from '../api/storyData';
 
-function StoryCard({ storyObj }) {
+function StoryCard({ storyObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
+  const deleteThisStory = () => {
+    if (window.confirm(`Delete ${storyObj.title}?`)) {
+      deleteStory(storyObj.id).then(() => onUpdate());
+    }
+  };
+
   return (
     <Card className="card" style={{ width: '18rem', margin: '10px' }}>
       <Card.Img variant="top" src={storyObj.image} alt={storyObj.title} style={{ height: '400px' }} />
@@ -18,12 +27,16 @@ function StoryCard({ storyObj }) {
         <Link href={`/stories/${storyObj.id}/edit`} passHref>
           <Button variant="info">EDIT</Button>
         </Link>
+        <Button variant="danger" onClick={deleteThisStory} className="m-2">
+          DELETE
+        </Button>
       </Card.Body>
     </Card>
   );
 }
 
 StoryCard.propTypes = {
+  onUpdate: PropTypes.shape.isRequired,
   storyObj: PropTypes.shape({
     image: PropTypes.string,
     title: PropTypes.string,
