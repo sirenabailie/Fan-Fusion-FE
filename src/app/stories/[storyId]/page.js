bc41ecaee484dc1648f5d20c913c82f28c2507f1
sirenabailie <sirenabailie@gmail.com>
2024-10-25 21:44:59 -0500

    merge

--- src/app/stories/[storyId]/page.js
+++ src/app/stories/[storyId]/page.js
@@ -7,16 +7,10 @@ import PropTypes from 'prop-types';
 export default function ViewStory({ params }) {
   const [storyDetails, setStoryDetails] = useState({});
 
-  // const { storyId } = params;
-
   useEffect(() => {
+    // Fetch story details including tags
     getSingleStory(params.storyId).then(setStoryDetails);
   }, [params.storyId]);
-  console.warn(storyDetails);
-
-  if (!storyDetails) {
-    return <p>Loading...</p>; // Loading state until story data is fetched
-  }
 
   return (
     <div className="mt-5 d-flex flex-wrap justify-content-center">
@@ -24,14 +18,27 @@ export default function ViewStory({ params }) {
       <div className="d-flex flex-column">
         <img src={storyDetails.image} alt={storyDetails.title} style={{ width: '300px' }} />
       </div>
-
-      {/* Story Details */}
       <div className="text-white ms-5 details">
         <h5>
           {storyDetails.title} by {storyDetails.user?.username || 'Unknown author'}
         </h5>
-        {/* Story Description */}
         <p>{storyDetails.description || 'No description available'}</p>
+
+        {/* Display Tags */}
+        {storyDetails.tags?.length > 0 ? (
+          <div>
+            <h6>Tags:</h6>
+            <ul className="list-inline">
+              {storyDetails.tags.map((tag) => (
+                <li key={tag.id} className="list-inline-item">
+                  <span className="badge bg-primary">{tag.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No tags available</p> // Handle case where no tags exist
+        )}
       </div>
     </div>
   );
