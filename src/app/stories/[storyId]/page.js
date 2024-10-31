8962bfeb36f9eb7cbd6df195c6b4bb770aa896d3
sirenabailie <sirenabailie@gmail.com>
2024-10-30 21:37:55 -0500

    story detail margin

--- src/app/stories/[storyId]/page.js
+++ src/app/stories/[storyId]/page.js
@@ -15,33 +15,46 @@ export default function ViewStory({ params }) {
   }, [params.storyId]);
 
   return (
-    <div className="mt-5 d-flex flex-wrap justify-content-center">
+    <div className="mt-5 d-flex justify-content-center">
+      {/* Image Section */}
       <div className="d-flex flex-column">
         <img src={storyDetails.image} alt={storyDetails.title} style={{ width: '300px' }} />
       </div>
-      <div className="text-white ms-5 details">
-        <h5 style={{ fontSize: '24px', fontWeight: 'bold' }}>
-          {storyDetails.title} by {storyDetails.user?.username || 'Unknown author'}
-        </h5>
-        <p>{storyDetails.description || 'No description available'}</p>
 
-        {storyDetails.tags?.length > 0 ? (
-          <div>
-            <h6>Tags:</h6>
-            <ul className="list-inline">
-              {storyDetails.tags.map((tag) => (
-                <li key={tag.id} className="list-inline-item">
-                  <span className="badge bg-primary">{tag.name}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>No tags available</p>
-        )}
+      {/* Story Details Section */}
+      <div className="text-white ms-5 d-flex flex-column" style={{ maxWidth: '500px' }}>
+        {/* Title and Author */}
+        <div className="mb-2">
+          <h5 style={{ fontSize: '24px', fontWeight: 'bold' }}>
+            {storyDetails.title} by {storyDetails.user?.username || 'Unknown author'}
+          </h5>
+        </div>
 
-        {/* Pass chapters to TableOfContents */}
-        {storyDetails.chapters && <TableOfContents storyId={params.storyId} chapters={storyDetails.chapters} />}
+        {/* Description */}
+        <div className="mb-4">
+          <p>{storyDetails.description || 'No description available'}</p>
+        </div>
+
+        {/* Table of Contents */}
+        <div className="mb-4">{storyDetails.chapters && <TableOfContents storyId={params.storyId} chapters={storyDetails.chapters} />}</div>
+
+        {/* Tags */}
+        <div>
+          {storyDetails.tags?.length > 0 ? (
+            <div>
+              <h6>Tags:</h6>
+              <ul className="list-inline">
+                {storyDetails.tags.map((tag) => (
+                  <li key={tag.id} className="list-inline-item">
+                    <span className="badge bg-primary">{tag.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No tags available</p>
+          )}
+        </div>
       </div>
     </div>
   );
