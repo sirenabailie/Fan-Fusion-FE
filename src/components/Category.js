d38d35bca3a16a9d4240d5a6e4685ad560d7f732
Casey <dinnerdoggy@gmail.com>
2024-11-02 12:53:28 -0500

    category buttons routing properly

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+// import { useRouter } from 'next/navigation';
 
 function CategoryCard({ categoryObj }) {
+  //  const router = useRouter();
+
+  // const handleCategoryClick = (categoryId) => {
+  //   router.push(`/categories/${categoryId}`);
+  // };
+
   return (
     <Card>
       <Card.Body>{categoryObj.title}</Card.Body>
+      <Button href={`/categories/${categoryObj.id}`}>click me</Button>
     </Card>
   );
 }
@@ -12,6 +21,7 @@ function CategoryCard({ categoryObj }) {
 CategoryCard.propTypes = {
   categoryObj: PropTypes.shape({
     title: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 
